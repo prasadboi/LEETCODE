e54c6c6db88d430a1ccdb94b335a273cfdec6b7a
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-04-24 12:49:34 +0530

    Time: 1159 ms (5.01%), Space: 43.5 MB (36.21%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -7,13 +7,11 @@ public:
     vvpii graph;
     vb visited;
     vi entry_time;
-    vi exit_time;
     
     void dfs(int src, int time)
     {
-        entry_time[src] = exit_time[src] = time;
+        entry_time[src] = time;
         visited[src] = true;
-        // cout<<"marked src = "<<src<<" as true";
         for(auto i : graph[src])
         {
             int node = i.first;
@@ -31,7 +29,6 @@ public:
         graph.resize(n);
         visited.resize(n, false);
         entry_time.resize(n, 0);
-        exit_time.resize(n, 0);
         
         for(auto i : times)
         {
@@ -39,8 +36,8 @@ public:
         }
         dfs(k-1, 0);        
         for(auto i : visited) if(!i) return -1; 
-        int max_exit = 0;
-        for(auto i : exit_time) max_exit = max(max_exit, i);
-        return max_exit;
+        int max_t = 0;
+        for(auto i : entry_time) max_t = max(max_t, i);
+        return max_t;
     }
 };
