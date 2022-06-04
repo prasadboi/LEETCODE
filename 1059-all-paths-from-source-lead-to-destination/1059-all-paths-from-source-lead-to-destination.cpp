d2ea81be11178a3cbe3a5d34b4f461fd70e56b6b
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-06-04 22:45:07 +0530

    Time: 100 ms (59.62%), Space: 33.1 MB (37.12%) - LeetHub

--- 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.cpp
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>> &al, vector<int> &visited, int i, int dest) {
-  if (al[i].empty()) return i == dest;
-  if (visited[i] != -1) return visited[i];
-  visited[i] = 0;
-  for (auto j : al[i])
-    if (!dfs(al, visited, j, dest)) return false;
-  return visited[i] = true;
-}
-bool leadsToDestination(int n, vector<vector<int>>& edges, int source, int destination) {
-  vector<vector<int>> al(n);
-  vector<int> visited(n, -1);
-  for (auto e : edges) al[e[0]].push_back(e[1]);
-  return dfs(al, visited, source, destination);
-}
+    bool dfs(vector<vector<int>> &graph, vector<int> &visited, int i, int dest) 
+    {
+        if (graph[i].empty()) return i == dest;
+        if (visited[i] != -1) return visited[i];
+        visited[i] = 0;
+        for (auto j : graph[i])
+            if (!dfs(graph, visited, j, dest)) return false;
+        return visited[i] = true;
+    }
+    bool leadsToDestination(int n, vector<vector<int>>& edges, int source, int destination) 
+    {
+        vector<vector<int>> graph(n);
+        vector<int> visited(n, -1);
+        for (auto e : edges) graph[e[0]].push_back(e[1]);
+        return dfs(graph, visited, source, destination);
+    }
 };
