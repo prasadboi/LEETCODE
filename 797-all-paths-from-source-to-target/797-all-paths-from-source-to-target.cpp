bb16b4a7be2e6ad7a9889bf81c45267cf34a852d
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-06-06 16:58:15 +0530

    Time: 71 ms (11.65%), Space: 17.8 MB (16.22%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    
+//     via dfs
+    
+    // void dfs(int s, int d, vector<vector<int>> &ans, vector<int> path, vector<bool> visited, vector<vector<int>> &graph)
+    // {
+    //     path.push_back(s);
+    //     if(s == d) ans.push_back(path);
+    //     visited[s] = true;
+    //     for(auto i : graph[s])
+    //     {
+    //         if(!visited[i]) dfs(i, d, ans, path, visited, graph);
+    //     }
+    //     visited[s] = false;
+    // }
+    
+//     via bfs
+    
+    void bfs(int src, int dest, vector<vector<int>> &ans, vector<int> path, vector<bool> visited, vector<vector<int>> &graph)
+    {
+        // as it is a directed graph, we do not need to use the visited array
+        // queue here will maintain the paths
+        queue<vector<int>> q;
+        
+        path = {src};
+        q.push(path);
+        while(!q.empty())
+        {
+            path = q.front(); q.pop();
+            int currNode = path[path.size() - 1];
+            if(currNode == dest)
+                ans.push_back(path);
+            else
+            {
+                for(auto i : graph[currNode])
+                {
+                    vector<int> temp(path.begin(), path.end());
+                    temp.push_back(i);
+                    q.push(temp);
+                }
+            }
+        }
+    }
+    
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) 
+    {
+        vector<vector<int>> ans;
+        vector<bool> visited(graph.size(), false);
+        vector<int> path;
+        // dfs(0, graph.size()-1, ans, path, visited, graph);
+        bfs(0, graph.size() - 1, ans, path, visited, graph);
+        return ans;
+    }
+};
