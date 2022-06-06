891f9269aa8d2b59d63769d7fc08fd3541ffcb6b
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-06-07 01:32:28 +0530

    Time: 3 ms (90.52%), Space: 9.5 MB (69.14%) - LeetHub

---
+++ 269-alien-dictionary/269-alien-dictionary.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+    #define ll long long int
+    #define vvll vector<vector<ll>>
+    #define vll vector<ll>
+    #define vb vector<bool>
+    
+    void print_graph(unordered_map<char, string> graph)
+    {
+        for(auto i : graph)
+        {
+            cout<<i.first<<"->"<<i.second<<endl;
+        }
+    }
+    
+    string alienOrder(vector<string>& words) 
+    {
+        unordered_map<char, string> graph;
+        unordered_map<char, int> indegree;
+        
+        // cout<<"check\n";
+        if(words.size() == 1)
+        {
+            for(auto i : words[0]) indegree[i] += 0;
+        }
+        for(int i = 0; i < words.size() - 1; i++)
+        {
+            ll n = min(words[i].size(), words[i+1].size());
+            int j = 0;
+            bool flag = false;
+            for(j = 0; j < n; j++)
+            {
+                // cout<<"words[i][j] = "<<words[i][j]<<" -- words[i+1][j] = "<<words[i+1][j]<<endl;
+                if(words[i][j] == words[i+1][j]){
+                    graph[words[i][j]] += "";
+                    indegree[words[i][j]] += 0;
+                    indegree[words[i+1][j]] += 0;
+                }
+                else{
+                    graph[words[i][j]] += words[i+1][j];
+                    graph[words[i+1][j]] += "";
+                    indegree[words[i][j]] += 0;
+                    indegree[words[i+1][j]] += 1;
+                    flag = true;
+                    j++;
+                    break;
+                }
+            }
+            if((words[i].size() - j > words[i+1].size() - j) and flag == false) return "";
+            int temp = j;
+            for(; j < words[i].size(); j++){
+                // cout<<"words[i][j]"<<words[i][j]<<endl;
+                graph[words[i][j]] += ""; indegree[words[i][j]] += 0;
+            }
+            for(j = temp; j < words[i+1].size(); j++)
+            {
+                // cout<<"words[i+1][j] = "<<words[i+1][j]<<endl;
+                graph[words[i+1][j]] += ""; indegree[words[i+1][j]] += 0;
+            }
+        }
+        
+        // print_graph(graph);
+        // for(auto i : indegree) cout<<i.first<<"--"<<i.second<<endl;
+        queue<char> q;
+        string res = "";
+        int ctr = 0;
+        for(auto i : indegree)
+        {
+            if(i.second == 0 and i.first >= 'a' and i.first <= 'z') q.push(i.first);
+            if(i.first >= 'a' and i.first <= 'z') ctr++;
+        }
+        // cout<<"check\n";
+        while(!q.empty())
+        {
+            char u = q.front(); q.pop();
+            res += u;
+            // cout<<"res : "<<res<<endl;
+            for(auto v : graph[u])
+            {
+                if(--indegree[v] == 0) q.push(v);
+            }
+        }
+        if(res.size() != ctr) return "";
+        else return res;
+    }
+};
