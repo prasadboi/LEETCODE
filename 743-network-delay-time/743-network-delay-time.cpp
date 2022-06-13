27a6dd2a6694714081af393cc25e0dd44e7881ab
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-06-13 14:51:33 +0530

    Time: 162 ms (58.10%), Space: 46.7 MB (15.45%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,43 +1,49 @@
 class Solution {
 public:
-    #define vvpii vector<vector<pair<int, int>>>
-    #define vi vector<int>
+    #define ll long long int
+    #define vll vector<ll>
+    #define vvll vector<vll>
     #define vb vector<bool>
+    #define vi vector<int>
+    #define pll pair<ll, ll>
+    #define vpll vector<pll>
+    #define vvpll vector<vpll>
+    #define INF 1e9 + 7
     
-    vvpii graph;
-    vb visited;
-    vi entry_time;
     
-    void dfs(int src, int time)
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) 
     {
-        entry_time[src] = time;
-        visited[src] = true;
-        for(auto i : graph[src])
-        {
-            int node = i.first;
-            int delay = i.second;
-            if(!visited[node]){
-                dfs(node, time + delay);
-            }
-            else if(time + delay < entry_time[node]){
-                dfs(node, time + delay);
-            }
-        }
-    }
-    
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        graph.resize(n);
-        visited.resize(n, false);
-        entry_time.resize(n, 0);
-        
+        vvpll graph(n);
         for(auto i : times)
         {
             graph[i[0]-1].push_back({i[1]-1, i[2]});
         }
-        dfs(k-1, 0);        
-        for(auto i : visited) if(!i) return -1; 
-        int max_t = 0;
-        for(auto i : entry_time) max_t = max(max_t, i);
-        return max_t;
+        
+        vll distance(n, INF);
+        set<pll> q;
+        
+        q.insert({0, k-1});
+        distance[k-1] = 0;
+        while(!q.empty())
+        {
+            ll u = q.begin()->second;
+            q.erase(q.begin());
+            for(auto it : graph[u]){
+                ll v = it.first, wt = it.second;
+                if(distance[v] > distance[u] + wt)
+                {
+                    q.erase({distance[v], v});
+                    distance[v] = distance[u] + wt;
+                    q.insert({distance[v], v});
+                }
+            }
+        }
+        
+        ll maxi = INT_MIN;
+        for(auto i : distance)
+        {
+            maxi = max(maxi, i);
+        }
+        return (maxi == INF) ? -1 : maxi;
     }
 };
