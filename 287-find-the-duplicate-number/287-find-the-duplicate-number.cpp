725ec103bfe385d0075664b8558d306ef2ff8167
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-06-18 22:30:06 +0530

    Time: 175 ms (52.38%), Space: 61.3 MB (61.19%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         int fast = nums[0], slow = nums[0];
-        fast = nums[nums[fast]];
-        slow = nums[slow];
-        while(fast != slow)
+        // floyd warsahll algorithm
+        do
         {
             fast = nums[nums[fast]];
             slow = nums[slow];
-        }
+        }while(fast != slow);
+        
         slow = nums[0];
         while(fast != slow){
             fast = nums[fast];
