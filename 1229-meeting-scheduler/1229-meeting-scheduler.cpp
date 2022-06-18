75af9cf7fe7c45ba22ad1edd15236a268c9b2265
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-06-18 21:48:58 +0530

    Time: 1883 ms (5.16%), Space: 201.3 MB (5.35%) - LeetHub

---
+++ 1229-meeting-scheduler/1229-meeting-scheduler.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    static bool cmp(vector<int> a, vector<int> b)
+    {
+        return a[1] < b[1];
+    }
+    
+    vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration)
+    {
+        if(!slots1.size() or !slots2.size()) return {};
+        sort(slots1.begin(), slots1.end(), cmp);
+        sort(slots2.begin(), slots2.end(), cmp);
+        
+        // for(auto i : slots1) cout<<i[0]<<"-"<<i[1]<<" "; cout<<endl;
+        // for(auto i : slots2) cout<<i[0]<<"-"<<i[1]<<" "; cout<<endl;
+        
+        int s1 = 0, s2 = 0;
+        for(; s1 < slots1.size() and s2 < slots2.size();)
+        {
+            if(min(slots1[s1][0], slots2[s2][0]) >= max(slots1[s1][1], slots2[s2][1])){
+                // the intervals do NOT overlap
+                if(slots1[s1][1] >= slots2[s2][1]) s1++;
+                else s2++;
+            }
+            else
+            {
+                // there must be some overlap area
+                int overlap = min(slots1[s1][1], slots2[s2][1]) - max(slots1[s1][0], slots2[s2][0]);
+                if(overlap >= duration)
+                    return {max(slots1[s1][0], slots2[s2][0]), max(slots1[s1][0], slots2[s2][0]) + duration};
+                else{
+                    if(slots1[s1][1] >= slots2[s2][1]) s2++;
+                    else s1++;
+                }
+            }
+        }
+        return {};
+        
+    }
+};
