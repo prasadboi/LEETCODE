7f79e5df49a7402179ef80c405c3e7e18d0a528c
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-06-30 23:44:05 +0530

    Time: 33 ms, Memory: 14.3 MB - LeetHub

--- course-schedule-ii/course-schedule-ii.cpp
+++ course-schedule-ii/course-schedule-ii.cpp
@@ -7,13 +7,6 @@ public:
     vi topOrder, indegree;
     vvi graph;
     
-    void dfs(int u){
-        topOrder.push_back(u);
-        indegree[u]--; // so that we dont traverse u again. ever
-        for(auto v : graph[u])
-            if(--indegree[v] == 0) dfs(v);
-    }
-    
     vector<int> findOrder(int n, vector<vector<int>>& edges) 
     {
         graph.resize(n, {});
@@ -22,10 +15,24 @@ public:
             graph[e[1]].push_back(e[0]);
             indegree[e[0]]++;
         }
-        for(auto i  = 0; i < n; i++) if(indegree[i] == 0) dfs(i);
         
-        if(topOrder.size() != n) return {};
-        else return topOrder;
+        ll ctr = 0;
+        queue<ll> q;
 
+        for(auto i = 0; i < n; i++) if(indegree[i] == 0) q.push(i);
+        while(!q.empty())
+        {
+            ll u = q.front(); q.pop();
+            topOrder.push_back(u);
+            for(auto v : graph[u]){
+                if(--indegree[v] == 0)
+                {
+                    q.push(v); 
+                }
+            }
+            ctr++;
+        }
+        if(ctr < n) return {};
+        else return topOrder;
     }
 };
