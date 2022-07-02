8d543294798c83c49b778c85a17499deb45cda94
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-02 22:31:55 +0530

    Time: 0 ms (100.00%), Space: 14.2 MB (7.47%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -37,9 +37,23 @@ ll top_down(ll i, vi arr){
     if(cost_dp_mem.find(i) != cost_dp_mem.end()) return cost_dp_mem[i];
     return cost_dp_mem[i] = min(top_down(i-1, arr) + arr[i], top_down(i-2, arr) + arr[i]);
 }
-//---------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------
+ll bottom_up(ll n, vi arr)
+{
+    vll cost_dp_tab(n, INF);
+    // BASE CASES:
+    // under the assumption that n >= 2 always
+    cost_dp_tab[0] = arr[0];
+    cost_dp_tab[1] = arr[1];
+    // DP
+    for(auto i = 2; i < n; i++) cost_dp_tab[i] = min(cost_dp_tab[i-1] + arr[i], cost_dp_tab[i-2] + arr[i]);
+    return min(cost_dp_tab[n-1], cost_dp_tab[n-2]);
+}
+//--------------------------------------------------------------------------------------
 
     int minCostClimbingStairs(vector<int>& cost) {
-        return (int) min(top_down(cost.size()-1, cost), top_down(cost.size()-2, cost));
+        // return (int) min(top_down(cost.size()-1, cost), top_down(cost.size()-2, cost));
+        return (int) bottom_up(cost.size(), cost);
     }
 };
