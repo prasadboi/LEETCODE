9e4cd346f56644f2a2a655066f0a80f8bd74a527
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-03 21:14:26 +0530

    Time: 71 ms (45.12%), Space: 8.1 MB (11.14%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -18,6 +18,19 @@ public:
     
     //--------------------------------------------------------------------------------------
     //--------------------------------------------------------------------------------------
+    // RECURSION APPROACH 1:
+    // jobDifficulty => JD[job number or job idx] = jobDifficulty_of_job_idx
+    // d => number of days in total
+        
+    // state variables :
+        // 1. d = the number of days left
+        // 2. the idx of the first job done on that particular day
+    // state : the difficulty of the job schedule given i days left and jth job being the first job on that day
+    
+    // rec rel : res = min([the cost for d-1 days with the job on those days starting on sched + 1] + 
+        // current_max JD for today (when there are d days left))
+    // for all k going from the next job idx to total_jobs left - 1
+    
     unordered_map<ll, umapll_ll> dp_mem;
     ll top_down(ll d, ll j, vi &JD, ll tot_d){
         // N = total number of jobs
@@ -41,44 +54,48 @@ public:
             // N - sched - 1 = number of jobs left
             // d-1 = the number of days left excluding today
             current_max = max(current_max, (ll) JD[sched]);
-            
-            // rec rel : res = min([the cost for d-1 days with the job on those days starting on sched + 1] + 
-            // current_max JD for today (when there are d days left))
-            // for all k going from the next job idx to total_jobs left - 1
             res = min(res, top_down(d-1, sched + 1, JD, tot_d) + current_max);
         }
         return dp_mem[d][j] = res;
     }
     
     //--------------------------------------------------------------------------------------
-    // --------------------------------------------------------------------------------------
-
-//     ll bottom_up(vi &JD, ll D)
-//     {
-//         ll N = JD.size();
+    //--------------------------------------------------------------------------------------
+    // RECURSION APPROACH 2:
+    // rec relation:
+    // function dp(i, d) : the minimum cost of job schedule which starts on job i on day d
+    vvll dp;
+    int bottom_up(vi &JD, ll tot_days)
+    {
+        ll num_jobs = JD.size();
+        dp.resize(num_jobs, vll (tot_days+1, INF));
         
-//         vvll dp_tab(D+1, vll(N+1, INF));
-//         dp_tab[D][N] = 0;
-//         for(auto i = 1; i < D+1; i++){
-//             for(auto j = 1; j < N+1; j++){
-//                 dp_tab[i][j]
-//             }
-//         }
-//     }
+        // BASE CASE:
+        dp[num_jobs - 1][tot_days] = JD[num_jobs-1];
+        // on the last day difficulty of day (for jobs from idx i to numjobs-1) 
+        for(auto i = num_jobs-2; i >= 0; i--) dp[i][tot_days] = max(dp[i+1][tot_days], (ll) JD[i]);
+        
+        for(auto day = tot_days - 1; day > 0; day--){
+            for(auto i = day-1; i < num_jobs - (tot_days - day); i++)
+            {
+                ll curr_max = 0;
+                for(auto j = i; j < num_jobs - (tot_days - day); j++){
+                    curr_max = max(curr_max, (ll)JD[j]);
+                    dp[i][day] = min(dp[i][day], dp[j+1][day+1] + curr_max); 
+                }
+            }
+        }
+        return (int) dp[0][1];
+        
+    }
     
     //--------------------------------------------------------------------------------------
     //--------------------------------------------------------------------------------------
 
     int minDifficulty(vi& jobDifficulty, int d) {
-        // jobDifficulty => JD[job number or job idx] = jobDifficulty_of_job_idx
-        // d => number of days in total
-        
-        // state variables :
-            // 1. d = the number of days left
-            // 2. the idx of the first job done on that particular day
-        // state : the difficulty of the job schedule given i days left and jth job being the first job on that day
         dp_mem.clear();
         if(jobDifficulty.size() < d) return -1;
-        return (int)top_down((ll)d, 0, jobDifficulty, (ll)d);
+        // return (int)top_down((ll)d, 0, jobDifficulty, (ll)d);
+        return bottom_up(jobDifficulty, (ll)d);
     }
 };
