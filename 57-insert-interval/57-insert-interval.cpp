bdb2d9bff812df9682254505af00a626187ce839
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-04 15:52:03 +0530

    Time: 29 ms (37.71%), Space: 17.1 MB (74.34%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -5,17 +5,18 @@ public:
     #define ll long long int
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         ll n = intervals.size();
+        // get idxs of element bfr and after the newInterval
         ll first_nxt = 0;
         while(first_nxt < n and intervals[first_nxt][0] <= newInterval[1]) first_nxt++;
         ll first_prev = first_nxt-1;
         while(first_prev >=0 and intervals[first_prev][1] >= newInterval[0]) first_prev--;
-        
-        cout<<"first_nxt = "<<first_nxt<<endl;
-        cout<<"first_prev = "<<first_prev<<endl;
-        
+                
+        // calculate the limits of the merged interval
         if(first_nxt-1 >= 0)newInterval[1] = max(newInterval[1], intervals[first_nxt-1][1]);
         if(first_prev + 1 < n) newInterval[0] = min(newInterval[0], intervals[first_prev + 1][0]);
         cout<<"newInterval : "<<newInterval[0]<<", "<<newInterval[1]<<endl;
+        
+        // create solution
         vvi res;
         for(auto i = 0; i <= first_prev; i++) res.push_back(intervals[i]);
         ll i;
