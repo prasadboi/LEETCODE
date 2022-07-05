8916686a41cc47292693a7900cd6901e0289cd57
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-05 09:13:39 +0530

    Time: 15 ms (18.80%), Space: 11.9 MB (43.13%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -18,7 +18,20 @@ public:
             return dp[i][hold_stock] = max(top_down(i + 1, 0, prices), -prices[i] + top_down(i+1, 1, prices));
     }
     
+    int bottom_up(vi &prices)
+    {
+        int n = prices.size();
+        vvi dp(n+2, vi (2, 0));
+        for(auto i = n-1; i >= 0; i--)
+        {
+            dp[i][1] = max(dp[i+1][1], dp[i+2][0] + prices[i]);
+            dp[i][0] = max(dp[i+1][0], dp[i+1][1] - prices[i]);
+        }
+        return dp[0][0];
+    }
+    
     int maxProfit(vector<int>& prices) {
-        return top_down(0, 0, prices);
+        // return top_down(0, 0, prices);
+        return bottom_up(prices);
     }
 };
