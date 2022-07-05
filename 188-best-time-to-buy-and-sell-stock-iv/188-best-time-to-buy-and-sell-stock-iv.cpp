4d1dae7c21f1bb7daa2cc101a511a1758b575adb
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-05 09:01:28 +0530

    Time: 52 ms (17.74%), Space: 21.3 MB (6.14%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -10,10 +10,30 @@ public:
     umapi_umapi_umapi_i dp;
     int top_down(ll i, ll rem_transactions, ll hold_stock, ll k, vi &prices)
     {
+        // base cases
         if(i == prices.size() or rem_transactions == 0) return 0;
-        if(dp[i][rem_transactions][hold_stock] != 0) return dp[i][rem_transactions][hold_stock];
-        if(hold_stock == 1) return dp[i][rem_transactions][hold_stock] = (int)max(top_down(i+1, rem_transactions, 1, k, prices), prices[i] + top_down(i+1, rem_transactions-1, 0, k, prices));
-        else return dp[i][rem_transactions][hold_stock] = (int)max(top_down(i+1, rem_transactions, 0, k, prices), top_down(i+1, rem_transactions, 1, k, prices) - prices[i]);
+        // memoization step
+        if(dp[i][rem_transactions][hold_stock] != 0) 
+            return dp[i][rem_transactions][hold_stock];
+        if(hold_stock == 1) // if you have stock then you can either sell it on day i or keep on holding to it
+            return dp[i][rem_transactions][hold_stock] = (int)max(top_down(i+1, rem_transactions, 1, k, prices), prices[i] + top_down(i+1, rem_transactions-1, 0, k, prices));
+        else // if you dont have any stock on day i, you can buy the stock on day i or do nothing
+            return dp[i][rem_transactions][hold_stock] = (int)max(top_down(i+1, rem_transactions, 0, k, prices), top_down(i+1, rem_transactions, 1, k, prices) - prices[i]);
+    }
+    
+    int bottom_up(ll k, vi &prices)
+    {
+        ll n = prices.size();
+        vector<vvi> dp(n+1, vvi (k+1, vi (2, 0)));
+        for(int i = n-1; i >= 0; i--)
+        {
+            for(int j = 1; j <= k; j++)
+            {
+                dp[i][j][0] = max(dp[i+1][j][1] - prices[i], dp[i+1][j][0]);
+                dp[i][j][1] = max(dp[i+1][j-1][0] + prices[i], dp[i+1][j][1]);
+            }
+        }
+        return (int)dp[0][k][0];
     }
     
     int maxProfit(int k, vector<int>& prices) 
@@ -30,6 +50,7 @@ public:
         and whether or not i currently hold stock
         */
         
-        return (int)top_down(0, (ll)k, 0, (ll)k, prices);
+        // return (int)top_down(0, (ll)k, 0, (ll)k, prices);
+        return bottom_up((ll)k, prices);
     }
 };
