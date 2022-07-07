27da7317ec50ab228caa1a4eac4831cb36c058d3
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-07 21:56:21 +0530

    Time: 7 ms (40.06%), Space: 8.7 MB (9.94%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    #define vi vector<int>
+    #define vvi vector<vi>
+    #define INF (1e9+7)
+    #define vb vector<bool>
+    
+    void permute(int i, vi temp, vvi &res, vb vis, vi &arr){
+        if(i == arr.size()){res.push_back(temp); return;}
+        for(auto j = 0; j < arr.size(); j++){
+            if(vis[j]) continue;
+            vis[j] = true;
+            temp.push_back(arr[j]);
+            permute(i+1, temp, res, vis, arr);
+            temp.pop_back();
+            vis[j] = false;
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vvi res;
+        vi temp;
+        vb vis(nums.size(), false);
+        permute(0, temp, res, vis, nums);
+        return res;
+    }
+};
