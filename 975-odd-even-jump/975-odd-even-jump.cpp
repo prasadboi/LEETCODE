31da7a9153df52742adff676ca37ecbdc62fd95b
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-09 02:39:40 +0530

    Time: 119 ms (74.24%), Space: 26.4 MB (82.14%) - LeetHub

--- 975-odd-even-jump/975-odd-even-jump.cpp
+++ 975-odd-even-jump/975-odd-even-jump.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     int oddEvenJumps(vector<int>& A) {
         int n  = A.size(), res = 1;
-        vector<int> higher(n), lower(n);
+        
+        vector<bool> higher(n, false), lower(n, false);
         higher[n - 1] = lower[n - 1] = 1;
+        
         map<int, int> map;
         map[A[n - 1]] = n - 1;
         for (int i = n - 2; i >= 0; --i) {
             auto hi = map.lower_bound(A[i]), lo = map.upper_bound(A[i]);
-            if (hi != map.end()) higher[i] = lower[hi->second];
-            if (lo != map.begin()) lower[i] = higher[(--lo)->second];
+            if (hi != map.end()) higher[i] = lower[hi->second]; // smallest value larger than target
+            if (lo != map.begin()) lower[i] = higher[(--lo)->second]; // largest index less than current value
+            
             if (higher[i]) res++;
             map[A[i]] = i;
         }
