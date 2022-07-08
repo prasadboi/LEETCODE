06700308015aed9cab29872f6dc2330f6e2ed9a3
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-09 01:56:25 +0530

    Time: 44 ms, Memory: 15.5 MB - LeetHub

--- unique-email-addresses/unique-email-addresses.cpp
+++ unique-email-addresses/unique-email-addresses.cpp
@@ -6,19 +6,19 @@ public:
         uset<string> mail;
         for(auto email : emails)
         {
-            cout<<"email : "<<email<<endl;
-            // ltrim(email); rtrim(email);
-            string username = email.substr(0, email.find("@"));
-            cout<<"username : "<<username<<endl;
+
             // process the username
-            username = username.substr(0, username.find("+"));
-            cout<<"valid part of username : "<<username<<endl;
+            /*get username*/string username = email.substr(0, email.find("@"));
+            /*remove everything after '+'*/username = username.substr(0, username.find("+"));
+            // remove all the '.'s
             char dot = '.';
             username.erase(remove(username.begin(), username.end(), dot), username.end());
-            cout<<"res username : "<<username<<endl;
+            
             // process the domain
+            // get the domain
             string domain = email.substr(email.find("@"), email.size() - email.find("@"));
             
+            // insert the processed email into the set
             mail.insert(username + "@" + domain);
         }
         return mail.size();
