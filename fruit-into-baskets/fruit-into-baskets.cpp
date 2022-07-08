2e98621a4a5e89f9cccb8ca5a268b4fe7dfa0259
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-09 03:22:10 +0530

    Time: 235 ms, Memory: 71.2 MB - LeetHub

---
+++ fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& tree) {
+        if(tree.size() == 0) return 0;
+        unordered_map<int, int> count;
+        int l, r;
+        int res = 1;
+        for (l = 0, r = 0; r < tree.size(); r++) {
+            count[tree[r]]++;
+            if (count.size() > 2) {
+                if (--count[tree[l]] == 0)count.erase(tree[l]);
+                l++;
+            }
+            if(count.size() <= 2) res = max(res, r - l + 1);
+        }
+        return  res;
+    }
+};
