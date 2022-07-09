6d5604933c0ececf601339a4ec1d028bf8449073
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-09 10:32:44 +0530

    Time: 45 ms (82.28%), Space: 89.7 MB (30.99%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -14,20 +14,27 @@ public:
         int lo = 0, hi = X.size();
         while(lo <= hi)
         {
-            int mx = lo + (hi - lo)/2;
-            int y = midlen - mx;
+            int mx = lo + (hi - lo)/2; // the midpoint of array X under cosideration
+            // the length of array Y split such that 
+            // X_L.size() + Y_L.size() == X_R.size() + Y_R.size()
+            int y = midlen - mx; 
+
             double maxLeftX = (mx == 0) ? INT_MIN : X[mx - 1];
             double minRightX = (mx == nx) ? INT_MAX : X[mx];
             
             double maxLeftY = (y == 0) ? INT_MIN : Y[y-1];
             double minRightY = (y == ny) ? INT_MAX : Y[y];
-            
-            if(maxLeftX <= minRightY and maxLeftY <= minRightX){
+
+            // median is possible when : 
+            if(maxLeftX <= minRightY and maxLeftY <= minRightX)
+            {
                 if((nx + ny)%2) return max(maxLeftX, maxLeftY);
                 else return (double)(max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2;
             }
+            // median lies to the left of the current mx
             else if(maxLeftX > minRightY) hi = mx-1;
-            else lo = mx  +1;
+            // median lies to the right of the current mx
+            else lo = mx + 1;
         }
         return 0;
     }
