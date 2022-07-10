c67082134cf6043adb05ee9843a5e78a360f7474
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-10 19:20:40 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (91.90%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -9,13 +9,9 @@ public:
         while(u <= d and l <= r)
         {
             for(int i = l; i <= r; i++) res.push_back(matrix[u][i]); u++;
-            cout<<"u = "<<u<<endl;
             for(int i = u; i <= d; i++) res.push_back(matrix[i][r]); r--;
-            cout<<"r = "<<r<<endl;
             for(int i = r; i >= l; i--) res.push_back(matrix[d][i]); d--;
-            cout<<"d = "<<d<<endl;
             for(int i = d; i >= u; i--) res.push_back(matrix[i][l]); l++;
-            cout<<"l = "<<l<<endl;
         }
         while(res.size() > m*n) res.pop_back();
         return res;
