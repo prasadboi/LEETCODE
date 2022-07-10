481f94fd6c6367362634828d1bcb015a4c72a38a
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-10 20:04:35 +0530

    Time: 28 ms (84.64%), Space: 7.2 MB (73.24%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -6,6 +6,8 @@ public:
         int max_len = 1; int maxStr = 0;
         for(int i = 0; i < s.size(); i++)    
         {
+            // at each position there are 2 possibilities : 
+            // either ....a....
             if(i > 0 and i < n-1 and s[i-1] == s[i+1])
             {
                 int l = i-1, r = i+1, len = 1;
@@ -15,6 +17,7 @@ public:
                 max_len = len;
                 }
             }
+            // or ....aa....
             if(i > 0 and s[i-1] == s[i]){
                 int l = i-1, r = i, len = 0;
                 while(l > -1 and r < n and s[l] == s[r]) l--,r++, len+=2;
