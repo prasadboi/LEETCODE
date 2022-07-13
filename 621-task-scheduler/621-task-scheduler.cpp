1614cffcfe3644ff03b3680432d4380c86e4e2c1
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-13 11:36:43 +0530

    Time: 75 ms (90.57%), Space: 34.4 MB (92.98%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        unordered_map<char, int> M;
-        int maxfreq = 0;
-        for(auto i : tasks){M[i]++; maxfreq = max(maxfreq, M[i]);}
-        int ans = (maxfreq-1)*(n+1);
-        for(auto i : M) if(i.second == maxfreq) ans++;
-        return max((int)tasks.size(), ans);
+        vector<int> freq(26, 0);
+        for(auto i : tasks) freq[i - 'A']++;
+        sort(freq.begin(), freq.end());
+        int fmax = freq[25]; freq.pop_back();
+        int idle_time = (fmax-1)*(n);
+        while(idle_time and freq.size())
+        {
+            idle_time -= min(fmax - 1, freq.back());
+            freq.pop_back();
+            idle_time = max(0, idle_time);
+        }
+        return idle_time + tasks.size();
     }
 };
