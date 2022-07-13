19343735d577768c8cb93fd8e3e896de182be978
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-14 01:15:36 +0530

    Time: 14 ms (9.96%), Space: 12.2 MB (36.60%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -7,12 +7,20 @@ public:
         for(auto i=n-2; i>=0; i--)
             if(nums[i] < nums[i+1]){k = i; break;}
         
+        // the array is sorted in descending order already
         if(k == -1){reverse(nums.begin(), nums.end()); return;}
         
+        // we found a case where nums[k] < nums[k+1]
         if(k >= 0){
             int j = n-1;
+            // find the furthest away index to the right that has value larger than nums[k];
+            // this must be true other wise we would have had k == -1
             while(nums[j] <= nums[k]) j--;
+            // swap the 2 numbers
             swap(nums[j], nums[k]);
+            // since everything to the right of j was in descending order the least permutation 
+            // greater than nums is the reverse version of that RHS of j (ascending fashion ie)
+            //(post swapping)
             reverse(nums.begin() + k + 1, nums.end());
         }
         
