bdad613c06199f64156fe9798d04b49fda9a5e6a
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-14 23:15:41 -0400

    Time: 557 ms (72.43%), Space: 165.2 MB (71.54%) - LeetHub

--- 146-lru-cache/146-lru-cache.cpp
+++ 146-lru-cache/146-lru-cache.cpp
@@ -1,72 +1,42 @@
-/*
 class LRUCache {
 public:
-//  LEAST RECENTLY USED CACHE
-    unordered_map<int, int> cache;
-    map<int, int> LRU;
-    int cap;
-    int ctr;
+    #define ll long long int
+    #define umap unordered_map
+    
+    list<pair<ll, ll>> LRU; // stores the LRU item in a list style
+    umap<ll, list<pair<ll, ll>>::iterator> cache; // stores the list iterator assoc with a key
+    ll cap;
     LRUCache(int capacity) {
         cap = capacity;
-        ctr = 0;
     }
     
     int get(int key) {
         if(cache.find(key) != cache.end()){
-            LRU[key] = ctr++;
-            return cache[key];
+            // the key is available
+            LRU.splice(LRU.begin(), LRU, cache[key]);
+            return cache[key]->second;
         }
         else return -1;
     }
     
-    void put(int key, int value) {
-        // cout<<"pushed : "<<key<<" - "<<value<<endl;
-        cache[key] = value;
-        LRU[key] = ctr++;
-        int lru_id = min_element(LRU.begin(), LRU.end(), [](const auto &a, const auto &b){return a.second < b.second;})->first;
-        if(cache.size() <= cap) return;
-        // cout<<"lruid is : "<<lru_id<<endl;
-        cache.erase(lru_id);
-        LRU.erase(lru_id);
-    }
-};
-
-*/
-
-class LRUCache {
-public:
-    list<pair<int, int>> LRU;
-    map<int, list<pair<int, int>>::iterator> cache; // cache[k] points to an iterator of a list
-    int cap;
-    
-    LRUCache(int capacity) {
-        LRU.clear();
-        cache.clear();
-        cap = capacity;
-    }
-    
-    int get(int key) {
-        if(cache.find(key) == cache.end()) return -1;
-        else{
-            LRU.splice(LRU.begin(), LRU, cache[key]);
-            // cout<<"found key : "<<cache[key]->second<<endl;
-            return cache[key]->second;
-        }
-    }
-    
-    void put(int key, int value) {
-        // cout<<"putting key - value"<<key<<"-"<<value<<endl;
+    void put(int key, int value) 
+    {
         if(cache.find(key) != cache.end()){
-            LRU.splice(LRU.begin(), LRU, cache[key]);
+            // if the key isin the LRU range then simply update value and make it most recently used item
             cache[key]->second = value;
+            LRU.splice(LRU.begin(), LRU, cache[key]);
         }
-        else{
+        else
+        {
+            // if key has not been encountered earlier
             if(cache.size() == cap){
-                auto delete_key = LRU.back().first;
-                LRU.pop_back();
-                cache.erase(delete_key);
+                // first make space for entering one key-val pair into the list
+                pair<ll, ll> LRU_item = LRU.back(); LRU.pop_back();
+                // if not in list then must not be in cache also
+                cache.erase(LRU_item.first);
             }
-            LRU.push_front({key, value});
+            pair<ll, ll> new_item = {key, value};
+            LRU.push_front(new_item);
             cache[key] = LRU.begin();
         }
     }
