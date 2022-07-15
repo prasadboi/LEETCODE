64aab4a1a3c5e96c0ffe930edd35a0d5e24b8935
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-15 16:24:36 -0400

    Time: 11 ms (60.68%), Space: 13.8 MB (72.12%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -8,7 +8,8 @@ public:
         for(auto i = 1; i < nums.size(); i++){
             ll maxPSF = maxProdSoFar, minPSF = minProdSoFar;
             maxProdSoFar = max({maxPSF*nums[i], minPSF*nums[i], (ll)nums[i]});
-            minProdSoFar = min({maxPSF*nums[i], minPSF*nums[i], (ll)nums[i]});
+            // cant use the maxProdSoFar vaariable as it has already been updated            
+            minProdSoFar = min({maxPSF*nums[i], minPSF*nums[i], (ll)nums[i]}); 
             res = max(res, maxProdSoFar);
         }
         return res;
