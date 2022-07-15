e1d410af6d727274e06b7a37f0286d6aa4c82a72
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-14 23:51:50 -0400

    Time: 3 ms (54.07%), Space: 8 MB (5.92%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -14,10 +14,26 @@ public:
     {
         if(i == -1) return 0;
         if(i == 0) return nums[0];
+        // NOTE : dp.find(i) is a faster operation than dp[i] = 0;
         if(dp.find(i) != dp.end()) return dp[i];
         return dp[i] = max(top_down(i-1, nums), top_down(i-2, nums) + nums[i]);
     }
+    
+    ll bottom_up(vi &nums)
+    {
+        ll n = nums.size();
+        vll dp(n, 0);
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(auto i = 2; i < n; i++)
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+        return dp[n-1];
+    }
+    
     int rob(vector<int>& nums) {
-        return (int) top_down(nums.size()-1, nums);
+        // return (int) top_down(nums.size()-1, nums);
+        return (int)bottom_up(nums);
     }
 };
