66322bd2df2de5647d03bdd6b571215626a7c870
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-21 01:08:29 -0400

    Time: 89 ms (69.74%), Space: 48.4 MB (43.19%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,42 @@
+/*
+// O(N^2) solution:
+// (T.L.E.)
+class Solution {
+public:
+    #define vb vector<bool>
+    #define vi vector<int>
+    #define vvi vector<vi>
+    #define ll long long int
+    
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vb dp(n, false); dp[0] = true;
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(i - j <= nums[j]) dp[i] = dp[i] or dp[j];
+            }
+        }
+        return dp[n-1];
+    }
+};
+*/
+
+class Solution {
+public:
+    #define vb vector<bool>
+    #define vi vector<int>
+    #define vvi vector<vi>
+    #define ll long long int
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return true;
+        int jump = 0; // maxindex we can jump to
+        for(int i = 0; i < n; i++){
+            jump = max(jump, i + nums[i]);
+            if(jump == i and i < n-1) return false;
+        }
+        return true;
+    }
+};
