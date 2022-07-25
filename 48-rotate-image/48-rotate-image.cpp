cb68eb62fb39a47eabaf1a45f3b498914f0e4619
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-25 13:07:40 -0400

    Time: 4 ms (42.28%), Space: 7.2 MB (33.20%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
@@ -13,5 +14,22 @@ public:
                 matrix[i][j] = temp;
             }
         }
+    }
+};
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(auto i = matrix.begin(); i != matrix.end(); i++) reverse((*i).begin(), (*i).end());
+        for(auto i = 0; i <= n; i++)
+        {
+            for(auto j = 0; j < n - i; j++)
+            {
+                swap(matrix[i][j], matrix[n-1-j][n-1-i]);
+            }
+        }
+        
     }
 };
