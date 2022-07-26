d1c9ac612dd0cad4fe8967051e64d4942b456f4f
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-26 08:22:18 -0400

    Time: 25 ms (37.64%), Space: 7 MB (52.71%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -4,11 +4,14 @@ public:
     int characterReplacement(string s, int k) {
         int res = 0, l = 0, r = 0, n = s.size();
         umap<char, int> freq;
-        int  maxFreq = 0;
+        // for a given window the window size >= (k + the max freq char in that window)
         for(; r < n; r++)
         {
             freq[s[r]]++;
-            maxFreq = max(maxFreq, freq[s[r]]);
+            int maxFreq = 0;
+            for(auto i : freq) maxFreq = max(maxFreq, i.second); // stores the max freq of the char in that window
+            // you could also keep track of the max freq char by 
+            // iterating over the ffreq hashmap and finding the max freq char for the window
             while((r - l + 1) - maxFreq > k)
                 freq[s[l]]--, l++;
             res = max(res, r - l + 1);
