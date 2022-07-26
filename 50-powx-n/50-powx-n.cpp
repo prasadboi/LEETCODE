a966fc249e5e165134c6cd173d40d6dd8f93f24b
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-26 02:29:24 -0400

    Time: 3 ms (47.21%), Space: 6 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        if(n < 0) return (myPow(1/x, abs(n)));
-        if(n == 0) return 1;
+    #define ll long long int
+    double myPow(double x, int num) {
+        long double res = 1;
+        ll n = num;
+        if(x == 1) return 1;
+        if(n < 0) x = 1/x;
+        (n < 0) ? (n = -1*n) : n = n;
+        cout<<"x = "<<x<<", n = "<<n<<endl;
+        while(n){
+            if(n&1) res *= x;
+            n = n>>1;
+            x = x*x;
+        }
         
-        long double val = myPow(x, n/2);
-        if(n%2) return val*val*x;
-        else return val*val;
+        return (double)(res);
     }
 };
