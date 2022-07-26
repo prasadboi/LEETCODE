e2ca9103744559ec3adfddfbe4208cf8c34a2092
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-26 14:32:32 -0400

    Time: 292 ms (39.95%), Space: 65.2 MB (26.59%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,43 +1,50 @@
+/*
+1. Store the startTime, endTime and profit of each job in jobs.
+2. Sort the jobs according to their starting time.
+3. Iterate over jobs from left to right, where position is the index of the current job. 
+For each job, we must compare two options:
+    i. Skip the current job (earn 0 profit) and move on to consider the job at the index 
+    position + 1.
+    ii. Schedule the current job (earn profit for the current job) and move on to the next 
+    non-conflicting job whose index is nextIndex. nextIndex is determined by using binary 
+    search in the startTime array.
+4. Return the maximum profit of the two choices and record this profit in the array memo (memoization).
+*/
+
+
 class Solution {
 public:
     // maximum number of jobs are 50000
     int memo[50001];
     
-    int findMaxProfit(vector<int>& startTime, vector<vector<int>>& jobs) {
-        int length = startTime.size();
-        
-        for (int position = length - 1; position >= 0; position--) {
-            // it is the profit made by scheduling the current job 
-            int currProfit = 0;
-            
-            // nextIndex is the index of next non-conflicting job
-            // this step is similar to the binary search as in the java solution
-            // lower_bound will return the iterator to the first element which is 
-            // equal to or greater than the element at `jobs[position][1]`
-            int nextIndex = lower_bound(startTime.begin(), startTime.end(), jobs[position][1]) - startTime.begin();
-            
-            // if there is a non-conflicting job possible add it's profit
-            // else just consider the curent job profit
-            if (nextIndex != length) {
-                currProfit = jobs[position][2] + memo[nextIndex];
-            } else {
-                currProfit = jobs[position][2];
-            }
-            
-            // storing the maximum profit we can achieve by scheduling 
-            // non - conflicting jobs from index i to the end of array
-            if (position == length - 1) {
-                memo[position] = currProfit;
-            } else {
-                memo[position] = max(currProfit, memo[position + 1]);
-            }
+    int findMaxProfit(vector<vector<int>>& jobs, vector<int>& start, int n, int position) {
+        // 0 profit if we have already iterated over all the jobs
+        if (position == n) {
+            return 0;
         }
+        // we have already calculated the answer so no need to go into recursion
+        if (memo[position] != -1) {
+            return memo[position];
+        }
+        // nextIndex is the index of next non-conflicting job
+        // this step is similar to the binary search as in the java solution
+        // lower_bound will return the iterator to the first element which is 
+        // equal to or greater than the element at `jobs[position][1]`
+        int nextIndex = lower_bound(start.begin(), start.end(), jobs[position][1]) - start.begin();
         
-        return memo[0];
+        // find the maximum profit of our two options: skipping or scheduling the current job
+        int maxProfit = max(findMaxProfit(jobs, start, n, position + 1), 
+                        jobs[position][2] + findMaxProfit(jobs, start, n, nextIndex));
+        
+        // returning maximum profit and also storing it for future reference (memoization)
+        return memo[position] = maxProfit;
     }
     
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         vector<vector<int>> jobs;
+        // marking all values to -1 so that we can differentiate 
+        // if we have already calculated the answer or not
+        memset(memo, -1, sizeof(memo));
         
         // storing job's details into one list 
         // this will help in sorting the jobs while maintaining the other parameters
@@ -52,6 +59,6 @@ public:
             startTime[i] = jobs[i][0];
         }
         
-        return findMaxProfit(startTime, jobs);
+        return findMaxProfit(jobs, startTime, profit.size(), 0);
     }
 };
