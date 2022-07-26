7d55891d3bd61475847b6aae27516f67c100de32
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-26 14:22:11 -0400

    Time: 244 ms (56.88%), Space: 49.9 MB (35.46%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -8,7 +8,7 @@ public:
         for(auto i : nums) seq.insert(i);
         
         int res = 0;
-        for(auto i : seq)
+        for(auto &i : seq)
         {
             int len = 1;
             for(int j = i; seq.find(j-1) != seq.end(); j--){seq.erase(j-1); len++;}
