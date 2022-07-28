396bf40827461c367375a07249a2e7c48ea6e3d9
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-28 11:31:59 -0400

    Time: 0 ms (100.00%), Space: 6.1 MB (40.25%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.cpp
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -1,36 +1,49 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int , char>> pq;
-        if(a> 0)                         // Pushing all non-zero occurances in queue
-        pq.push({a, 'a'});
-        if(b>0)
-        pq.push({b, 'b'});
-        if(c>0)
-        pq.push({c, 'c'});
-        string ans = "";
-        int size = 0;                    //size of resultant string
-        while(!pq.empty()){
-            auto it = pq.top();          //pop character with higest occurance
-            pq.pop();
-            if(size>1 && ans[size-1] == it.second && ans[size-2] == it.second){  //if there are already two consecutive occurances hold that character and pop the next character and put it 
-                if(pq.empty())return ans;
-                ans += pq.top().second;
-                //cout<<ans<<"\n";
-                auto tp = pq.top();
-                size++;
-                pq.pop();
-                if(tp.first >1)
-                pq.push({tp.first-1, tp.second});               // push the characters back into queue
-                pq.push({it.first, it.second});
+        //using max heap
+        priority_queue<pair<int,char>>pq;
+        if(a)
+        pq.push({a,'a'});
+        if(b)
+        pq.push({b,'b'});
+        if(c)
+        pq.push({c,'c'});
+        string ans="";
+        while(pq.size()>1){
+            pair<int,char>one = pq.top();pq.pop();
+            pair<int,char>two = pq.top();pq.pop();
+            if(one.first>=2){
+                ans+=one.second;
+                ans+=one.second;
+                one.first-=2;
             }
-            else {                                   //otherwise put the character and push it again.
-                ans += it.second;
-                size++;
-                if(it.first > 1)
-                pq.push({it.first-1, it.second});
+            else{
+                ans+=one.second;
+                one.first-=1;
             }
-            
+            if(two.first>=2 && two.first>=one.first){
+                ans+=two.second;
+                ans+=two.second;
+                two.first-=2;
+            }
+            else{
+                ans+=two.second;
+                two.first-=1;
+            }
+            if(one.first>0)
+                pq.push(one);
+            if(two.first>0)
+                pq.push(two);
+        }
+        if(pq.empty())
+            return ans;
+        if(pq.top().first>=2){
+            ans+=pq.top().second;
+            ans+=pq.top().second;
+        }
+        else{
+            ans+=pq.top().second;
         }
         return ans;
         
