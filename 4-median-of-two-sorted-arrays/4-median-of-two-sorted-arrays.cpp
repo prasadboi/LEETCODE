3c084deab1aa53eb48ff41738a9d160560af7607
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-07-29 14:04:32 +0530

    Time: 39 ms (92.37%), Space: 89.8 MB (30.80%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -8,33 +8,30 @@ public:
     
     double findMedianSortedArrays(vector<int> X, vector<int>& Y) 
     {
-        if(X.size() > Y.size()) return findMedianSortedArrays(Y, X);
-        
-        int nx = X.size(), ny = Y.size(), midlen = ceil((double)(nx + ny)/2);
-        int lo = 0, hi = X.size();
+        ll nx = X.size(), ny = Y.size(), midLen = ceil((double)(nx + ny)/2);
+        if(nx > ny) return findMedianSortedArrays(Y, X);
+        ll lo = 0, hi = nx;
         while(lo <= hi)
         {
-            int mx = lo + (hi - lo)/2; // the midpoint of array X under cosideration
-            // the length of array Y split such that 
-            // X_L.size() + Y_L.size() == X_R.size() + Y_R.size()
-            int y = midlen - mx; 
-
-            double maxLeftX = (mx == 0) ? INT_MIN : X[mx - 1];
-            double minRightX = (mx == nx) ? INT_MAX : X[mx];
+            // say we want to split the 2 arrays such that there are equal number of numbers on both sides
+            // therfore mx + my = midLen;
+            ll mx = lo + (hi - lo)/2;
+            ll my = midLen - mx;
             
-            double maxLeftY = (y == 0) ? INT_MIN : Y[y-1];
-            double minRightY = (y == ny) ? INT_MAX : Y[y];
-
-            // median is possible when : 
-            if(maxLeftX <= minRightY and maxLeftY <= minRightX)
-            {
-                if((nx + ny)%2) return max(maxLeftX, maxLeftY);
-                else return (double)(max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2;
-            }
-            // median lies to the left of the current mx
-            else if(maxLeftX > minRightY) hi = mx-1;
-            // median lies to the right of the current mx
-            else lo = mx + 1;
+            // now there are 4 numbers to consider
+            double X_lmax = (mx == 0) ? INT_MIN : X[mx-1], X_rmin = (mx == nx) ? INT_MAX : X[mx];
+            double Y_lmax = (my == 0) ? INT_MIN : Y[my-1], Y_rmin = (my == ny) ? INT_MAX : Y[my];
+            
+            
+            if(X_lmax <= Y_rmin and Y_lmax <= X_rmin){
+                // median lies between these 4 numbers
+                if((nx + ny)%2) return (double) max(X_lmax, Y_lmax);
+                else return (double)(max(X_lmax, Y_lmax) + min(X_rmin, Y_rmin))/2;
+            }           
+            else if(X_lmax > Y_rmin) // median lies to the left of m
+                hi = mx-1;
+            // median lies to the right of m
+            else lo = mx+1;
         }
         return 0;
     }
