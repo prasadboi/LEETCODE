b710d78fb6e8f4f1e1d1569cb8a438416191db8f
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-08-01 12:03:48 +0530

    Time: 151 ms (5.01%), Space: 44.4 MB (5.07%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    #define ll long long int
+    #define umap unordered_map
+    
+    
+    int minSubArrayLen(int target, vector<int>& nums) {
+        map<ll, ll> prefix;
+        prefix[0] = -1;
+        ll sum = 0, res = INT_MAX;
+        for(ll i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            auto it = prefix.lower_bound(sum - target);
+            if(it->first == sum - target){
+                res = min(res, i - it->second);
+            }
+            else{
+                if(it != prefix.end() and it->first > sum - target and it != prefix.begin()){
+                    // cout<<"it->sfirst = "<<it->first<<endl;
+                    --it;
+                    res = min(res, i - it->second);
+                }
+                else if(it == prefix.end()) res = min(res, i + 1);
+            }
+            prefix[sum] = i;
+        }
+        return (res == INT_MAX) ? 0 : res;
+    }
+};
