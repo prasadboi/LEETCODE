03258abd77a16b9f502f0a5c92816afdccd0411d
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-08-01 11:46:10 +0530

    Time: 591 ms (12.52%), Space: 91.8 MB (44.04%) - LeetHub

---
+++ 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    #define ll long long int
+    #define umap unordered_map
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        umap<ll, ll> prefix;
+        ll sum = 0, res = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            // cout<<"i = "<<i<<", sum = "<<sum<<endl;
+            if(sum == k){
+                // cout<<"i = "<<i<<", sum = "<<sum<<endl;
+                res = max(res, (ll)i + 1);
+            }
+            else if(prefix.find(sum - k) != prefix.end()) {
+                // cout<<"i = "<<i<<", sum = "<<sum<<", sum - k at : "<<prefix[sum-k]<<endl;
+                res = max(res, (ll)i - prefix[sum-k]);
+            }
+            if(prefix.find(sum) == prefix.end()) {
+                // cout<<"i = "<<i<<", new sum = "<<sum<<endl;
+                prefix[sum]=  i;
+            }
+        }
+        return res;
+    }
+};
