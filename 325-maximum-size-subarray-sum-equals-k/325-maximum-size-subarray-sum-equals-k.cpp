741c0a58a7e64421646a2f0f5057e718934cd133
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-08-01 11:50:18 +0530

    Time: 415 ms (58.15%), Space: 91.7 MB (55.47%) - LeetHub

--- 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.cpp
+++ 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.cpp
@@ -9,17 +9,13 @@ public:
         for(int i = 0; i < nums.size(); i++)
         {
             sum += nums[i];
-            // cout<<"i = "<<i<<", sum = "<<sum<<endl;
             if(sum == k){
-                // cout<<"i = "<<i<<", sum = "<<sum<<endl;
                 res = max(res, (ll)i + 1);
             }
             else if(prefix.count(sum - k) > 0) {
-                // cout<<"i = "<<i<<", sum = "<<sum<<", sum - k at : "<<prefix[sum-k]<<endl;
                 res = max(res, (ll)i - prefix[sum-k]);
             }
             if(prefix.count(sum) <= 0) {
-                // cout<<"i = "<<i<<", new sum = "<<sum<<endl;
                 prefix[sum]=  i;
             }
         }
