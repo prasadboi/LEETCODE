35293c7093f7a80eabd2cd2cb333dcf4b4e93953
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-08-03 15:09:12 +0530

    Time: 7 ms (46.21%), Space: 10.2 MB (71.91%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     #define ll long long int
+    bool isValley(vector<int> &nums, ll idx){
+        bool left = (nums[idx] <= nums[(idx-1 + nums.size())%nums.size()]);
+        bool right = nums[idx] <= nums[(idx + 1)%nums.size()];
+        return (left and right);
+    }
     
     int findMin(vector<int>& nums) 
     {
-        // using template 3 because i have to look at both sides (atleast that is my logic)
-        ll n = nums.size(), l = 0, r = n-1;
+        // template 3 binary  search
+        ll l = 0, n = nums.size(), r = n-1;
         while(l + 1 < r)
         {
             ll m = l + (r-l)/2;
-            if(nums[(n + m-1)%n] >= nums[m] and nums[m] <= nums[(m+1)%n]) return nums[m];
+            if(isValley(nums, m)) return nums[m];
             else if(nums[m] > nums[r]) l = m;
             else r = m;
         }
-        
-        if(nums[l] <= nums[(l+1)%n] and nums[l] <= nums[(n+l-1)%n]) return nums[l];
-        if(nums[r] <= nums[(r+1)%n] and nums[r] <= nums[(n+r-1)%n]) return nums[r];
+        if(isValley(nums, l)) return nums[l];
+        if(isValley(nums, r)) return nums[r];
         return -1;
     }
 };
