83a15cc876a84180ede1beac03281ea34d5d6658
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-08-04 11:58:29 +0530

    Time: 719 ms (15.49%), Space: 89.2 MB (10.26%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    #define vi vector<int>
-    #define vvi vector<vi>
     #define ll long long int
-    #define pll pair<ll, ll>
+    #define vll vector<ll>
+    #define umap unordered_map
+    #define uset unordered_set
+    #define pq priority_queue
     
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
-    {
-        auto cmp = [](const vi &a, const vi &b){
-            ll da = a[0]*a[0] + a[1]*a[1];
-            ll db = b[0]*b[0] + b[1]*b[1];
-            if(da != db) return da < db;
-            else return a <= b;
-        };
-        // set<pll> dist;
-        // for(int i = 0; i < points.size(); i++){
-        //     dist.insert({points[i][0]*points[i][0] + points[i][1]*points[i][1], i});
-        // }
-        vvi res;
-        // for(int i = 0; i < k and dist.size(); i++){
-        //     res.push_back(points[dist.begin()->second]);
-        //     dist.erase(dist.begin());
-        // }
-        set<vi, decltype(cmp)> s(cmp);
-        for(auto i : points) s.insert(i);
-        for(auto i = 0; i < k and s.size(); i++){res.push_back(*s.begin()); s.erase(s.begin());}
+    
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        auto cmp = [](vll &a, vll &b){return a[2] > b[2];};
+        pq<vll, vector<vll>, decltype(cmp)> mindist(cmp); 
+        for(auto i : points)
+            mindist.push({i[0], i[1], i[0]*i[0] + i[1]*i[1]});
+        
+        vector<vector<int>> res;
+        while(mindist.size() and k--){
+            vll x = mindist.top(); mindist.pop();
+            res.push_back({(int)x[0], (int)x[1]});
+        }
         return res;
     }
 };
