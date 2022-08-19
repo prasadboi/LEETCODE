a981ddf31fea8f8a2dfe1d9f1e5b09da388c2745
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-08-19 17:41:43 +0530

    Time: 5 ms (39.92%), Space: 8.6 MB (78.60%) - LeetHub

---
+++ 369-plus-one-linked-list/369-plus-one-linked-list.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head)
+    {
+        ListNode* iter = head, *prev = NULL, *real_next;
+        while(iter)
+        {
+            real_next = iter->next;
+            iter->next = prev;
+            prev = iter;
+            iter = real_next;
+        }
+        return prev;
+    }
+    
+    
+    ListNode* plusOne(ListNode* head) {
+        head = reverse(head);
+        ListNode* iter = head, *prev = NULL;
+        int c = 1;
+        while(iter)
+        {
+            iter->val += c;
+            c = iter->val/10;
+            iter->val = iter->val%10;
+            prev = iter;
+            iter = iter->next;
+        }
+        if(c == 1) prev->next = new ListNode(c);
+        return reverse(head);
+    }
+};
