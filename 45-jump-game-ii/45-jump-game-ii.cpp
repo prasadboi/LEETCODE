48b54ed65847cbc57fc86716efcc4a6fa10298fd
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-08-21 16:11:09 +0530

    Time: 24 ms (72.03%), Space: 16.5 MB (96.72%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
+    // sol using DP : O(n^2) time, O(n) space
+    // 
+    // int jump(vector<int>& nums) {
+    //     vector<long long int> dp(nums.size(), INT_MAX);
+    //     dp[0] = 0;
+    //     for(auto i = 0; i < nums.size()-1; i++)
+    //     {
+    //         for(auto j = 0; j <= nums[i]; j++){
+    //             if(i + j < nums.size()) dp[i+j] = min(dp[i+j], dp[i] + 1);
+    //         }
+    //     }
+    //     return dp[nums.size()-1];
+    // }
+    
     int jump(vector<int>& nums) {
-        vector<long long int> dp(nums.size(), INT_MAX);
-        dp[0] = 0;
-        for(auto i = 0; i < nums.size()-1; i++)
+        int maxReachable = 0, i = 0, lastPos = 0, ctr = 0;
+        for(; i < nums.size()-1; i++)
         {
-            for(auto j = 0; j <= nums[i]; j++){
-                if(i + j < nums.size()) dp[i+j] = min(dp[i+j], dp[i] + 1);
+            maxReachable = max(maxReachable, nums[i] + i);
+            if(i == lastPos){
+                lastPos = max(lastPos, maxReachable);
+                ctr++;
             }
         }
-        return dp[nums.size()-1];
+        return ctr;
     }
 };
