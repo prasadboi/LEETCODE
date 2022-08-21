ca49f4ec106033c79da1fb63b224c385cf08c6c5
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-08-21 19:36:34 +0530

    Time: 1066 ms (26.48%), Space: 272.9 MB (5.03%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,56 +1,63 @@
 class Solution {
 public:
+    
     #define ll long long int
+    #define umap unordered_map
     #define vll vector<ll>
     #define vvll vector<vll>
-    #define umap unordered_map
-    #define vb vector<bool>
-    #define vi vector<int>
     
-    
-    // i : the current day
-    // j : the number of transactions completed
-    // k : whether we are holding stock currently or not
-    // top_down(i, j) : the maximum profit made when we have completed i days and j transactions
-    
-    umap<ll, umap<ll, umap<bool, ll>>> dp;
-    
-    ll top_down(ll i, ll j, bool hold, vi &prices){
+    umap<ll, umap<ll, umap<ll, ll>>> dp;
+    ll top_down(ll i, ll j, ll k, vector<int> &prices){
+        if(j == 0) return 0;
         if(i >= prices.size()) return 0;
-        if(j <= 0) return 0;
-        if(dp[i][j][hold] != 0) return dp[i][j][hold];
+        if(dp.count(i) and dp[i].count(j) and dp[i][j].count(k)) return dp[i][j][k];
         ll res = 0;
-        if(hold == true){
-            // can either sell or hold on to it
-            res = max(top_down(i+1, j-1, false, prices) + prices[i], top_down(i+1, j, true, prices));
+        if(k == 1){
+            // holding stock:
+            // can either hold or sell today
+            res = max(top_down(i+1, j, k, prices), top_down(i+1, j-1, 0, prices) + prices[i]);
         }
-        else{
-            // can either buy or not buy
-            res = max(top_down(i+1, j, true, prices) - prices[i], top_down(i+1, j, false, prices));
+        else if(k == 0)
+        {
+            // not currently holding stock
+            // can either buy or forego today
+            res = max(top_down(i+1, j, 0, prices), top_down(i+1, j, 1, prices) - prices[i]);
         }
-        // cout<<"i = "<<i<<", j = "<<j<<", hold = "<<hold<<"top down = "<<res<<endl;
-        return dp[i][j][hold] = res;
+        return dp[i][j][k] = res;
     }
     
-    ll bottom_up(vi &prices)
+    ll bottom_up(vector<int> &prices)
     {
         ll n = prices.size();
-        vector<vvll> dp(n+1, vvll (3, vll(2, 0)));
+        vector<vvll> dp(n+1, vvll(3, vll(2, -1)));
+        for(auto i = 0; i < 3; i++) for(auto j = 0; j < 2; j++) dp[n][i][j] = 0;
+        for(auto i = 0; i <= n; i++) for(auto k = 0; k < 2; k++) dp[i][0][k] = 0;
+        
         for(auto i = n-1; i >= 0; i--)
         {
-            for(auto j = 2; j > 0; j--)
+            for(auto j = 1; j < 3; j++)
             {
-                
-                (dp[i][j][1] = max(dp[i+1][j][1], dp[i+1][j-1][0] + prices[i]));
-                (dp[i][j][0] = max(dp[i+1][j][0], dp[i+1][j][1] - prices[i]));
+                // not holding stock
+                dp[i][j][0] = max(dp[i+1][j][1] - prices[i], dp[i+1][j][0]);
+                dp[i][j][1] = max(dp[i+1][j][1], dp[i+1][j-1][0] + prices[i]);
             }
         }
-        return dp[0][2][0];
+        return dp[0][2][0]; // on the first day i dont have any pre stock, i have j days left
     }
     
     int maxProfit(vector<int>& prices) {
-        ll n = prices.size(), maxTransactions = 2;
-        // return top_down(0, 2, false, prices);
+        // can complete at most 2 transactions
+        // aim : to find the max possible profit
+        
+        // states : 
+            // ith day
+            // j number of transactions left
+            // k whether or not i am holding stock
+        
+        // on every day i can either buy or leave a stock(if i dont hold any)
+        // or
+        // sell the stock or hold it further (if i already hold some stock)
+        // return top_down(0, 2, 0, prices);
         return bottom_up(prices);
     }
 };
