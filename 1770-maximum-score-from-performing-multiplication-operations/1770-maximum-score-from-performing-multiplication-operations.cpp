1c9e12c7245314343b82a9e9562de2b1d66b4b9a
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-09-16 11:57:50 +0530

    Time: 830 ms (31.38%), Space: 120.1 MB (66.36%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -7,17 +7,17 @@ public:
     #define vll vector<ll>
     #define vvll vector<vll>
     
-     vector<vector<int>> dp;
-    int solve(int i, int n, int j, vector<int> &nums, vector<int> &multipliers){
-        if (j == multipliers.size()) return 0;
+    vector<vector<int>> dp;
+    int solve(int i, int n, int j, vector<int> &nums, vector<int> &mul){
+        if (j == mul.size()) return 0;
         if (dp[i][j] != INT_MIN) return dp[i][j];
-        int left = solve(i + 1, n, j + 1, nums, multipliers) + (nums[i] * multipliers[j]);
-        int right = solve(i, n, j + 1, nums, multipliers) + (nums[(n - 1) - (j - i)] * multipliers[j]);
+        int left = solve(i + 1, n, j + 1, nums, mul) + (nums[i] * mul[j]);
+        int right = solve(i, n, j + 1, nums, mul) + (nums[(n - 1) - (j - i)] * mul[j]);
         return dp[i][j] = max(left, right);
     }
-    int maximumScore(vector<int>& nums, vector<int>& multipliers) {   
-        int n = nums.size(), m = multipliers.size();
+    int maximumScore(vector<int>& nums, vector<int>& mul) {   
+        int n = nums.size(), m = mul.size();
         dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
-        return solve(0, n, 0, nums, multipliers);
+        return solve(0, n, 0, nums, mul);
     }
 };
