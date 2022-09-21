cf3adaba3e2d1017cd5a2e1b23b7b82e4768c828
Arjun Prasad <55182416+prasadboi@users.noreply.github.com>
2022-09-21 23:25:55 +0530

    Time: 219 ms (46.12%), Space: 20 MB (72.20%) - LeetHub

---
+++ 1297-maximum-number-of-occurrences-of-a-substring/1297-maximum-number-of-occurrences-of-a-substring.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    #define umap unordered_map
+    #define uset unordered_set
+    #define ll long long int
+    #define vll vector<ll>
+    
+    int maxFreq(string s, int maxLetters, int minSize, int maxSize) {
+        // count number of substrings with 
+        // max unique letters  = maxLetters
+        // minSize = minSize
+        // maxSize = maxSize
+        umap<char, int> letters;
+        map<string, int> string_freq;
+        int res = 0;
+        int count = 0;
+        
+        for(auto i = 0; i < minSize; i++) letters[s[i]]++;
+        if(letters.size() <= maxLetters){
+            string x = s.substr(0, minSize);
+            string_freq[x]++;
+        }
+        
+        int l = 1, r = l + minSize - 1;
+        for(; r < s.size();)
+        {
+            letters[s[r]]++;
+            letters[s[l-1]]--;
+            if(letters[s[l-1]] == 0) letters.erase(s[l-1]);
+            if(letters.size() <= maxLetters)
+            {
+                string x = s.substr(l, r - l + 1);
+                string_freq[x]++;
+                res = max(res, string_freq[x]);
+            }
+            l++, r++;
+        }
+        return res;
+    }
+};
